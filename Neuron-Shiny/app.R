b6a3a8e83dfbbf0ce7a7148b0f1b9852c4479ede
Joey Kilgore <joey.a.kilgore@gmail.com>
2019-09-25 14:10:00 -0400

    Voltage Profile Viewer
    Using the sample Voltage.csv file, the app now displays the Voltage of a given node (mV) vs. Time (ms). The user can use the Node Number slider to choose which node is displayed on the plot, and the graph is updated automatically.

--- Neuron-Shiny/app.R
+++ Neuron-Shiny/app.R
@@ -8,26 +8,28 @@
 #
 
 library(shiny)
+source("UsefulFunctions.R")
+v <- csvToDF(read.csv("Data/Voltage.csv"))
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
 
     # Application title
-    titlePanel("Old Faithful Geyser Data"),
+    titlePanel("Voltage Plot"),
 
     # Sidebar with a slider input for number of bins 
     sidebarLayout(
         sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
+            sliderInput("nodeNum",
+                        "Node Number:",
                         min = 1,
-                        max = 50,
-                        value = 30)
+                        max = 101,
+                        value = 51)
         ),
 
         # Show a plot of the generated distribution
         mainPanel(
-           plotOutput("distPlot")
+           plotOutput("voltPlot")
         )
     )
 )
@@ -35,13 +37,12 @@ ui <- fluidPage(
 # Define server logic required to draw a histogram
 server <- function(input, output) {
 
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
+    output$voltPlot <- renderPlot({
+        ggplot(data=v)+
+            geom_path(aes(x=Time,y=v[,input$nodeNum+1]), color="#111111", size=2)+
+            ylab("Voltage (mV)")+
+            xlab("Time (ms)")+
+            ggtitle("10kHz Block Central Node Voltage")
     })
 }
 
