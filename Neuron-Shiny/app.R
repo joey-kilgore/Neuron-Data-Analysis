a975bb95b37db6c722350457632fbb991f49a15e
Joey Kilgore <joey.a.kilgore@gmail.com>
2019-09-30 10:46:59 -0400

    Added ability to save plot
    Plots are now saved by clicking the "Save" button in the left side panel. The plots are then saved in the Plots folder.

--- Neuron-Shiny/app.R
+++ Neuron-Shiny/app.R
@@ -9,22 +9,26 @@
 
 library(shiny)
 source("UsefulFunctions.R")
+
+# define data and plot
 v <- csvToDF(read.csv("Data/Voltage.csv"))
+vPlot <- ggplot()
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
-
     # Application title
     titlePanel("Voltage Plot"),
 
-    # Sidebar with a slider input for number of bins 
+    # Sidebar with a slider input and save button
     sidebarLayout(
         sidebarPanel(
             sliderInput("nodeNum",
                         "Node Number:",
                         min = 1,
                         max = 101,
-                        value = 51)
+                        value = 51),
+            br(),
+            actionButton("save", "Save Plot")
         ),
 
         # Show a plot of the generated distribution
@@ -36,13 +40,26 @@ ui <- fluidPage(
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-
+    # Render a new plot when inputs change
     output$voltPlot <- renderPlot({
-        ggplot(data=v)+
-            geom_path(aes(x=Time,y=v[,input$nodeNum+1]), color="#111111", size=2)+
+        print("GENERATING NEW PLOT")
+        vPlot <- ggplot(data=v)+
+            geom_path(aes(x=Time,y=v[,input$nodeNum+1]), color="#111111", size=1)+
             ylab("Voltage (mV)")+
             xlab("Time (ms)")+
-            ggtitle("10kHz Block Central Node Voltage")
+            ggtitle(sprintf("10kHz Block Node %d Voltage",input$nodeNum))
+        vPlot
+    })
+    
+    # Save the plot to the plots folder when button is clicked
+    observeEvent(input$save,{
+        print("SAVING")
+        plot <- ggplot(data=v)+
+            geom_path(aes(x=Time,y=v[,input$nodeNum+1]), color="#111111", size=1)+
+            ylab("Voltage (mV)")+
+            xlab("Time (ms)")+
+            ggtitle(sprintf("10kHz Block Node %d at BT",input$nodeNum))
+        ggsave(filename = sprintf("./Plots/voltage%d.png",input$nodeNum), plot, width = 5, height = 3, dpi=150)
     })
 }
 
