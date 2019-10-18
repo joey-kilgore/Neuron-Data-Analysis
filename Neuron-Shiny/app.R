2bf270849d48365a04d54615904a65607515b83d
joey-kilgore <joey.a.kilgore@gmail.com>
2019-10-18 15:10:42 -0400

    added multiple pages for the r shiny app A page is dedicated to viewing the voltage profile (maybe more later) A third page is dedicated for viewing 3d plots

--- Neuron-Shiny/app.R
+++ Neuron-Shiny/app.R
@@ -9,13 +9,14 @@
 
 library(shiny)
 library(ggplot2)
+library("plot3D")
 source("UsefulFunctions.R")
 
 # define data and plot
-DATA <- getDataAllDF("/mnt/c/Users/Joey/Desktop/InternalElectrode/Only Center Stim (NO ONSET)/")
-#dataAvg <- movingAverageDF(DATA,20)
+DATA <- getDataAllDF("/mnt/c/Users/Joey/Desktop/InternalElectrode/No DC/")
 keepPlot <- ggplot()    # Keeps current saved plot
 
+# MAIN PAGE FUNCTIONS (2D Plot)
 generateVector <- function(vectorVar, nodeNum, tStart, tStop, avgBool, avgNum){
     # generate the vector from DATA given the input parameters
     colNum = 0
@@ -40,13 +41,14 @@ generateVector <- function(vectorVar, nodeNum, tStart, tStop, avgBool, avgNum){
 
 generatePlot <- function(settings){
     # settings is a vector containing the following strings
-    # [1]   variable (v=Voltage, m=MGate, etc)
-    # [2]   node number
-    # [3]   tStart 
-    # [4]   tStop
-    # [5]   color
-    # [6]   avgNum
-    # [7]   avgBool
+    # [1]   xvar (v=Voltage, m=MGate, etc)
+    # [2]   yvar same as xvar
+    # [3]   node number
+    # [4]   tStart 
+    # [5]   tStop
+    # [6]   color
+    # [7]   avgNum
+    # [8]   avgBool
 
     cat("Current Settings: ")
     cat(settings)
@@ -55,22 +57,14 @@ generatePlot <- function(settings){
     plot <- keepPlot   # Build plot from last stored plot
 
     # Generate x axis data
-    if(settings[1] == "pp") {
-        vectorVar <- "m"
-    }else{
-        vectorVar <- "t"
-    }
-    xData <- generateVector(vectorVar, settings[2], settings[3], settings[4], settings[7], settings[6])
+    xData <- generateVector(settings[1], settings[3], settings[4], settings[5], settings[8], settings[7])
 
     # Generate y axis data
-    if(settings[1] == "pp") {
-        vectorVar <- "h"
-    }else{
-        vectorVar <- settings[1]
-    }
-    yData <- generateVector(vectorVar, settings[2], settings[3], settings[4], settings[7], settings[6])
+    yData <- generateVector(settings[2], settings[3], settings[4], settings[5], settings[8], settings[7])
 
-    plot <- plot+geom_path(aes(x=xData,y=yData), color=settings[5], size=1)
+    plot <- plot+geom_path(aes(x=xData,y=yData), color=settings[6], size=1)+
+                xlab(settings[1])+ylab(settings[2])+
+                ggtitle(sprintf("%s vs %s",settings[2],settings[1]))
         
     plot
 }
@@ -78,70 +72,139 @@ generatePlot <- function(settings){
 generateName <- function(settings){
     # Generate file name for a given plot settings
     # Using the form "<variable>_<nodeNum>.png"
-    newName <- sprintf("%s_%s.png",settings[1],settings[2])
+    newName <- sprintf("%s_vs_%s_Node%s.png",settings[2],settings[1],settings[3])
     newName
 }
 
-ui <- fluidPage(
-    fluidRow(
-        # Sidebar panel
-        column(2,
-            titlePanel("NEURON DATA"),
-            selectInput("variable", "Variable:",
-                        c("Voltage" = "v",
-                          "M Gate" = "m",
-                          "H Gate" = "h",
-                          "Phase Plane"="pp")),
-            fluidRow(
-                column(6,
-                    numericInput("nodeNum", "Node Number:", 51, min=1, max=101, step=1)
-                ),
-                column(6,
-                    numericInput("avgNum", "Moving AVG Width:", 20, min=1, max=100, step=1)
-                )
-            ),
-            checkboxInput("avgBool", "Turn on Moving Average:", FALSE),
-            selectInput("color", "Color:",c("BLACK"="#111111","RED"="#FF2222","BLUE"="#2222FF","GREEN"="#22FF22")),
-            br(),
-           
-            # Time start and stop selection, divs were used for styling to force the two on the same line
-            div(style="display: inline-block;vertical-align:center; width: 45%;",numericInput("tStart", "Start Time:", 0, min=0, max=100, step=.01)),
-            div(style="display: inline-block;vertical-align:center; width: 45%;",numericInput("tStop", "Stop Time:", 100, min=0, max=100, step=.01)),
-            br(),
-            
-            fluidRow(
-                column(6,
-                    actionButton("generate", "Generate")
-                ),
-                column(6,
-                    actionButton("keep", "Keep Plot")
-                )
-            ),
-            br(),
-            fluidRow(
-                column(6,
-                    actionButton("clear", "Clear Plot")
-                ),
-                column(6,
-                    actionButton("save", "Save Plot")
-                )
-            )
-        ),
+# PROFILE PLOT FUNCTIONS
+generateProfile <- function(tProfile){
+    # Generate the profile plot (V vs node) at a given time
+    x <- seq(1,101,by=1)
+    vData <- as.vector(unlist(DATA[as.numeric(tProfile)*200+1,x+203]))
+    plot <- ggplot() + geom_path(aes(x=x,y=vData), color="#EE2222", size=1)+
+                xlab("Node")+ylab("Voltage")+
+                xlim(1,101) +ylim(-120,60)+
+                ggtitle(sprintf("Voltage Profile @t=%f",DATA[as.numeric(tProfile)*200,1]))
+    plot
+}
 
-        # Main panel
-        column(10,
-            plotOutput("mainPlot", height=700)
+# 3D PLOT FUNCTIONS
+generate3D <- function(nodeNum, tStart, tStop, avgBool, avgNum, theta, phi){
+    # generate a 3d plot with x=m, y=h, z=v
+    x <- generateVector("m", nodeNum, tStart, tStop, avgBool, avgNum)
+    y <- generateVector("h", nodeNum, tStart, tStop, avgBool, avgNum)
+    z <- generateVector("v", nodeNum, tStart, tStop, avgBool, avgNum)
+    
+    plot <- scatter3D(x,y,z, bty="g", theta=as.numeric(theta), phi=as.numeric(phi), type="l", ticktype="detailed", lwd=2)
+    plot
+}
+
+ui <- navbarPage("Neuron Data",
+    tabPanel("2D Plot",
+        fluidRow(
+            # Sidebar panel
+            column(2,
+                selectInput("yvar", "Y Variable:",
+                            c("Voltage" = "v",
+                              "M Gate" = "m",
+                              "H Gate" = "h",
+                              "Time"="t")),
+                
+                selectInput("xvar", "X Variable:",
+                            c("Voltage" = "v",
+                              "M Gate" = "m",
+                              "H Gate" = "h",
+                              "Time" = "t")),
+                fluidRow(
+                    column(6,
+                        numericInput("nodeNum", "Node Number:", 51, min=1, max=101, step=1)
+                    ),
+                    column(6,
+                        numericInput("avgNum", "Moving AVG Width:", 20, min=1, max=100, step=1)
+                    )
+                ),
+                checkboxInput("avgBool", "Turn on Moving Average:", FALSE),
+                selectInput("color", "Color:",c("BLACK"="#111111","RED"="#FF2222","BLUE"="#2222FF","GREEN"="#22FF22")),
+                br(),
+               
+                # Time start and stop selection, divs were used for styling to force the two on the same line
+                div(style="display: inline-block;vertical-align:center; width: 45%;",numericInput("tStart", "Start Time:", 0, min=0, max=100, step=.01)),
+                div(style="display: inline-block;vertical-align:center; width: 45%;",numericInput("tStop", "Stop Time:", 100, min=0, max=100, step=.01)),
+                br(),
+                
+                fluidRow(
+                    column(6,
+                        actionButton("generate", "Generate")
+                    ),
+                    column(6,
+                        actionButton("keep", "Keep Plot")
+                    )
+                ),
+                br(),
+                fluidRow(
+                    column(6,
+                        actionButton("clear", "Clear Plot")
+                    ),
+                    column(6,
+                        actionButton("save", "Save Plot")
+                    )
+                )
+            ),
+
+            # Main panel
+            column(10,
+                plotOutput("mainPlot", height=600)
+            )
+        )
+    ),
+    tabPanel("V Profile",
+        fluidRow(
+            # Sidebar panel
+            column(2,
+                 numericInput("tProfile", "Time", 0, min=.005, max=100, step=.005)
+            ),
+            column(10,
+                plotOutput("VProfilePlot", height=600)
+            )
+        )
+    ),
+    tabPanel("3D Plot",
+        fluidRow(
+            # Sidebar panel
+            column(2,
+                numericInput("nodeNum3d", "Node Number:", 51, min=1, max=101, step=1),
+                numericInput("tStart3d", "Start Time:", .005, min=.005, max=100, step=.005),
+                numericInput("tStop3d", "Stop Time:", 100, min=.005, max=100, step=.005),
+                numericInput("avgNum3d", "Moving AVG Width:", 20, min=1, max=100, step=1),
+                checkboxInput("avgBool3d", "Turn on Moving AVG:", FALSE),
+                fluidRow(
+                    column(6,
+                        numericInput("theta","Theta",0,min=0,max=180,step=5)
+                    ),
+                    column(6,
+                        numericInput("phi","Phi",0,min=0,max=180,step=5)
+                    )
+                )
+            ),
+            column(10,
+                plotOutput("plot3d", height=600)
+            )
         )
     )
-)
+)   
 
 # Define server logic required to draw a histogram
 server <- function(input, output, session) {
-    # Render a new plot when inputs change
+    # MAIN PAGE (2D Plot)
+    generateSettings <- function(){
+        c(input$xvar,input$yvar,input$nodeNum,input$tStart,input$tStop,input$color,input$avgNum,input$avgBool)
+    }
+    
+    # Render new main plot when the generate button is clicked
     output$mainPlot <- renderPlot({
         input$generate
         cat("GENERATING NEW PLOT\n")
-        settings <- isolate(c(input$variable,input$nodeNum,input$tStart,input$tStop, input$color, input$avgNum, input$avgBool))
+        settings <- isolate(generateSettings())
         mainPlot <- isolate(generatePlot(settings))
         mainPlot
     })
@@ -149,8 +212,8 @@ server <- function(input, output, session) {
     # Save the plot to the plots folder when button is clicked
     observeEvent(input$save,{
         cat("SAVING\n")
-        plot <- generatePlot(c(input$variable, input$nodeNum, input$tStart, input$tStop,input$color, input$avgNum, input$avgBool))
-        plotName <- generateName(c(input$variable, input$nodeNum, input$tStart, input$tStop, input$color, input$avgNum, input$avgBool))
+        plot <- generatePlot(generateSettings())
+        plotName <- generateName(generateSettings())
         ggsave(filename = sprintf("./Plots/%s",plotName), plot, width = 5, height = 3, dpi=150)
     })
     
@@ -158,7 +221,7 @@ server <- function(input, output, session) {
     observeEvent(input$keep,{
         cat("KEEPING\n")
         # note that to overwrite session variables the '<<-' syntax is used
-        keepPlot <<- generatePlot(c(input$variable, input$nodeNum, input$tStart, input$tStop, input$color, input$avgNum, input$avgBool))
+        keepPlot <<- generatePlot(generateSettings())
     })
     
     # Clear the current layered plot
@@ -167,6 +230,18 @@ server <- function(input, output, session) {
         # note that to overwrite session variables the '<<-' syntax is used
         keepPlot <<- ggplot()
         mainPlot <-ggplot()
+    })
+
+    # VOLTAGE PROFILE
+    output$VProfilePlot <- renderPlot({
+        VProfilePlot <- generateProfile(input$tProfile)
+        VProfilePlot
+    })
+
+    # 3D PLOT
+    output$plot3d <- renderPlot({
+        plot3d <- generate3D(input$nodeNum3d,input$tStart3d,input$tStop3d,input$avgBool3d,input$avgNum3d,input$theta,input$phi)
+        plot3d
     })
 }
 
